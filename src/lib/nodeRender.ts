8a0655ce44006b3c585ee251c985e23dd37e80b7
莫荣灿 <morongcan@morongcandeMBP.lan>
2020-01-07 01:01:34 +0800

    debug

--- src/lib/nodeRender.ts
+++ src/lib/nodeRender.ts
@@ -1,9 +1,30 @@
 //
 //
 //
+import React from 'react'
+import * as antd from 'antd'
 
-const render = function(): string {
-  return '123'
+const data = [
+  {
+    id: '',
+    type: 'Button',
+    props: [
+      {
+        name: 'innerText',
+        value: 'hehe'
+      }
+    ]
+  }
+]
+
+const render = function(): [] {
+  let elements = []
+  data.forEach((elementObj: object) => {
+    const com: React.ComponentClass = antd['Button']
+    const element = React.createElement(com, {}, 'hehe')
+    elements.push(element)
+  })
+  return elements
 }
 
 export default render
